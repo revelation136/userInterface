048963b9e566bc9674635e2e9452d035bddc6e79
revelation136 <revelation136@gmail.com>
2022-05-14 09:33:38 +0800

    Infinite Scroll - appending script2.0 using async & await rather than .then

--- scroll/posts/static/posts/script2.0.js
+++ scroll/posts/static/posts/script2.0.js
@@ -2,24 +2,28 @@ let counter = 1;
 let quantity = 20;
 
 document.addEventListener('DOMContentLoaded', load_content);
-// window.onscroll = function () {
-//     if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
-//         load_content();
-//     }
-// };
+window.onscroll = function () {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+        load_content();
+    }
+};
 
-function load_content() {
+async function load_content() {
     const start = counter;
     const end = start + quantity - 1;
     counter = end + 1;
 
-    fetch(`/posts?start=${start}&end=${end}`).then(r => r.json()).then(data => console.log(data.xyz));
+    const res = await fetch(`/posts?start=${start}&end=${end}`);
+    const data = await res.json();
 
+    console.log(data.xyz);
+
+    data.xyz.forEach(add_post);
 }
 
-// function add_post(content) {
-//     const post = document.createElement('div');
-//     post.className = 'post';
-//     post.innerHTML = content;
-//     document.querySelector('#posts').append(post);
-// };.then(r => r))
+function add_post(content) {
+    const post = document.createElement('div');
+    post.className = 'post';
+    post.innerHTML = content;
+    document.querySelector('#posts').append(post);
+};
